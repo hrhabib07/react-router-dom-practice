a26adafaea12e4abdabd73149645e7a77ba88af5
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-07-31 14:55:50 +0600

    basic route setup completed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import Contact from "./pages/Contact.tsx";
+import Products from "./pages/Products.tsx";
+import NotFound from "./pages/NotFound.tsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/about",
+    element: <About></About>,
+  },
+  {
+    path: "/contact",
+    element: <Contact></Contact>,
+  },
+  {
+    path: "/products",
+    element: <Products></Products>,
+  },
+  {
+    path: "*",
+    element: <NotFound></NotFound>,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="my-12 flex items-center justify-center">
+        <h1 className="text-2xl font-semibold">
+          <span className="text-gray-900">404 --</span>{" "}
+          <span className="text-red-600">page not found!</span>{" "}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
