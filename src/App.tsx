e5676402215f8f4fe8fd969e4ac9eabc5278cee5
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-07-31 14:42:20 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+const App = () => {
+  return (
+    <div>
+      <Home></Home>
+      <Products></Products>
+      <Contact></Contact>
+      <About></About>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Hello, From Contact!</h1>
+    </div>
+  );
+};
+
+export default Contact;
