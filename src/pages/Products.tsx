04b5bb9e906c730118c531a5181ae64e568b698e
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-08-01 12:07:04 +0600

    simple pagination function implemented

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -9,26 +9,55 @@ const Products = () => {
   const [products, setProducts] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const productsPerPage = 9;
+  const [totalPages, setTotalPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+
   useEffect(() => {
     setIsLoading(true);
-    fetch("https://dummyjson.com/products")
+    fetch(
+      `https://dummyjson.com/products?limit=${productsPerPage}&skip=${
+        (currentPage - 1) * productsPerPage
+      }`
+    )
       .then((res) => res.json())
-      .then((data) => setProducts(data))
+      .then((data) => {
+        setProducts(data);
+        setTotalPages(Math.ceil(data.total / productsPerPage));
+      })
       .catch((error) => setError(error))
       .finally(() => setIsLoading(false));
-  }, []);
+  }, [currentPage]);
 
   return (
     <div>
       <h1>Hello, From Products!</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div>
         {isLoading && <p>Data is loading...</p>}
         {error && <p>{error}</p>}
-        {!isLoading &&
-          !error &&
-          products?.products?.map((product: TProduct) => (
-            <ProductCard product={product} key={product.id}></ProductCard>
-          ))}
+        {!isLoading && !error && (
+          <>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {products?.products?.map((product: TProduct) => (
+                <ProductCard product={product} key={product.id}></ProductCard>
+              ))}
+            </div>
+            <div className="my-4">
+              {Array.from({ length: totalPages }, (_, index) => {
+                return (
+                  <button
+                    key={index}
+                    className="mr-2 border rounded p-2 bg-gray-50"
+                    onClick={() => setCurrentPage(index + 1)}
+                  >
+                    {index + 1}
+                  </button>
+                );
+              })}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
