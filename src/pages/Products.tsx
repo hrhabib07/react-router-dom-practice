37afaf7ed935d851fbccd3a805e9ebcfd08f134a
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-08-01 12:36:45 +0600

    pagination design implemented and next page previous page first page last page implemented

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -44,17 +44,61 @@ const Products = () => {
               ))}
             </div>
             <div className="my-4">
+              <button
+                className={`mr-2  rounded p-2 bg-gray-50 ${
+                  currentPage === 1 && "text-gray-500"
+                }`}
+                onClick={() => setCurrentPage(1)}
+                disabled={currentPage === 1}
+                aria-label="First page"
+              >
+                &laquo; &laquo;
+              </button>
+              <button
+                className={`mr-2  rounded p-2 bg-gray-50 ${
+                  currentPage === 1 && "text-gray-500"
+                }`}
+                onClick={() => setCurrentPage(currentPage - 1)}
+                disabled={currentPage === 1}
+                aria-label="previous page"
+              >
+                &laquo;
+              </button>
               {Array.from({ length: totalPages }, (_, index) => {
                 return (
                   <button
                     key={index}
-                    className="mr-2 border rounded p-2 bg-gray-50"
+                    className={`mr-2 border rounded p-2  ${
+                      currentPage === index + 1
+                        ? "bg-blue-600 text-white"
+                        : "bg-gray-50"
+                    }`}
                     onClick={() => setCurrentPage(index + 1)}
                   >
                     {index + 1}
                   </button>
                 );
               })}
+              <button
+                className={`mr-2 rounded p-2 bg-gray-50 ${
+                  currentPage === totalPages && "text-gray-500"
+                }`}
+                onClick={() => setCurrentPage(currentPage - 1)}
+                disabled={currentPage === totalPages}
+                aria-label="next-page"
+              >
+                &raquo;
+              </button>
+              <button
+                className={`mr-2 rounded p-2 bg-gray-50 ${
+                  currentPage === totalPages && "text-gray-500"
+                }`}
+                onClick={() => setCurrentPage(totalPages)}
+                disabled={currentPage === totalPages}
+                aria-label="Last page"
+              >
+                &raquo; &raquo;
+              </button>
             </div>
           </>
         )}
